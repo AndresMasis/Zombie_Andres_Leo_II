001235a318db68f33e84325ff29fe3467042a99d
AndresMasis <67446603+AndresMasis@users.noreply.github.com>
2020-10-28 13:54:59 -0600

    Se subio el primer archivo del proyecto

---
+++ ProjectAndresLeo/src/projectandresleo/ProjectAndresLeo.java
@@ -0,0 +1,20 @@
+/*
+This is the code of the second project of the Object Oriented Programming Course
+*/
+
+package projectandresleo;
+
+/**
+ *
+ * @author Andrés
+ */
+public class ProjectAndresLeo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
